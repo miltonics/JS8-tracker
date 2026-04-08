5a6c46447032b07e252eddb3570d96233e2372b7
miltonics <miltonics@hotmail.com>
2026-04-08 10:35:13 -0400

    v0.3.2.1 — restore map view on deselect, remove duplicate files

--- venv/bin/activate.fish
+++ venv/bin/activate.fish
@@ -33,7 +33,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV /home/milton/ham/js8-tracker/venv
+set -gx VIRTUAL_ENV /home/milton/radio/js8-tracker/venv
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/"bin $PATH
